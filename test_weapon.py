1cd14a58ce8cec3af680256f09a12a2c57d5899a
flowerschaos <cfactionbackup@gmail.com>
2025-01-07 12:18:49 -0500

    test test

--- test_weapon.py
+++ test_weapon.py
@@ -1,10 +1,8 @@
 from weapon import Weapon
 from ranged import Ranged
 from melee import Melee
+from armory import brella
+from armory import revolver
 
-def test_weapon():
-    pass
-def test_ranged():
-    pass
-def test_melee():
-    pass
+brella.attack()
+revolver.attack()
